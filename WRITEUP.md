d510efe571045fd790922a95b7d89b683cb1906f
Giang™ <giang.phan271@outlook.com>
2024-10-07 02:43:54 +0700

    complete project

--- WRITEUP.md
+++ WRITEUP.md
@@ -5,9 +5,27 @@
 _For **both** a VM or App Service solution for the CMS app:_
 
 - _Analyze costs, scalability, availability, and workflow_
+  | | VM | App Service |
+  |--------------|--------------------------------------------------------------------------------------------------------------------------|-------------------------------------------------------------------------------------------------------------------------|
+  | **Cost** | Billing based on VM size, storage (OS, data, backups), licensing, networking, and other services | Billing based on service plan, number of instances, storage, network bandwidth, and additional services |
+  | **Scalability** | Scale Sets can increase/decrease the number of virtual machines or automatically change as needed | Automatically scale (add instances) or scale up (increase capacity) based on criteria such as CPU, memory, or number of requests |
+  | **Availability** | Guaranteed uptime of 99.9% when using Availability Set and up to 99.99% when using Availability Zones | Guaranteed 99.95% uptime for applications from Standard Tier and above |
+  | **Deployment** | Requires manual installation of operating systems, software, and environment configuration or using tools such as ARM templates, PowerShell, or Terraform | Deploy easily using tools like Azure Portal, CLI, Visual Studio, or through CI/CD pipelines |
+
 - _Choose the appropriate solution (VM or App Service) for deploying the app_
 - _Justify your choice_
+
+From my opinion and appropriate solution, both VM and App Service are good for our application, but I choose App Service for some reasons:
+
+- _This is fully manage platform_
+- _This is rapid deployment, support CI/CD_
+- _This is simplifies deploying applications across multiple regions for geo-redundancy and disaster recovery_
+- _And this is an ideal for modern application architectures, including microservices and serverless computing_
 
 ### Assess app changes that would change your decision.
 
 _Detail how the app and any other needs would have to change for you to change your decision in the last section._
+
+| VM                                                                                           | App Service                                                       |
+| -------------------------------------------------------------------------------------------- | ----------------------------------------------------------------- |
+| Control over environment like OS, software, configurations that are required for application | Modern App deployment (microservices, serverless,...) management. |
--- config.py
+++ config.py
@@ -5,17 +5,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'e1162f08-6084-4071-90ce-49d028d7a9a1'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'hellocmsstorage'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'FTLAj/gsNIF6rHQagiwZOVVIZm8Df4CZMQOCsNpVYLCNKr+Gd+kocAd7GfGXw8u+au2MtKydZY3E+AStaAd5aA=='
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
-    BLOB_CONNECTION_STRING = os.environ.get('BLOB_CONNECTION_STRING') or 'DefaultEndpointsProtocol=https;AccountName=hellocmsstorage;AccountKey=FTLAj/gsNIF6rHQagiwZOVVIZm8Df4CZMQOCsNpVYLCNKr+Gd+kocAd7GfGXw8u+au2MtKydZY3E+AStaAd5aA==;EndpointSuffix=core.windows.net'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT')
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY')
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER')
+    BLOB_CONNECTION_STRING = os.environ.get('BLOB_CONNECTION_STRING')
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'hello-cms-dbserver.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'hello-cms-db'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'cmsadmin'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or '123@Well'
+    SQL_SERVER = os.environ.get('SQL_SERVER')
+    SQL_DATABASE = os.environ.get('SQL_DATABASE')
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME')
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD')
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect={}".format(
         quote_plus(
@@ -29,7 +29,7 @@ class Config(object):
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = os.environ.get("CLIENT_SECRET") or 'Ja68Q~fQJyM5neE5.L0x_KNLt6dCNR1npPO4jdCp'
+    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
@@ -39,7 +39,7 @@ class Config(object):
 
     AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
 
-    CLIENT_ID = os.environ.get("CLIENT_ID") or '00fa1f63-9312-4dc7-b50a-e7093fad439b'
+    CLIENT_ID = os.environ.get("CLIENT_ID")
 
     REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
